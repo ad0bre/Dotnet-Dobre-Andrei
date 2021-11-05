f6648a3257633e4089a8ba1ba17024826449aed5
Dobre Andrei <andrei.dobr3@gmail.com>
2021-11-05 16:04:49 +0200

    TUTORIAL: Application Architecture
    Last video watched: Application Architecture

---
+++ Dobre-Andrei/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; //utilizeaza un namespace
+
+namespace Dobre_Andrei //structura de organizare
+{
+    class Program // clasa ce contine membre (ex: functie, constructor)
+    {
+        static void Main(string[] args) //functie, contine instructiuni
+        {
+            Console.WriteLine("Hello World!"); //instructiune (ce executa computerul)
+        }
+    }
+}
+//namespace -> clase -> functii -> instructiuni
