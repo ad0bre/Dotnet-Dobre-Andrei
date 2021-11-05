8896b2d75031813bbaee0eabfdbdfa80e709729a
Dobre Andrei <andrei.dobr3@gmail.com>
2021-11-05 19:15:36 +0200

    TUTORIAL: Switch
    Provided screenshot with the part of the video that I got to

---
+++ Dobre-Andrei/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dobre_Andrei
+{
+    public class Person
+    {
+        public string FirstName { get; set; } // proprietate care se poate citi/scrie
+        public string LastName { get; set; }
+        public string GetFullName() //functie care returneaza un sir de caractere
+        {
+            return FirstName + " " + LastName;
+        }
+
+    }
+}
--- Dobre-Andrei/Program.cs
+++ Dobre-Andrei/Program.cs
@@ -44,8 +44,75 @@ namespace Dobre_Andrei //structura de organizare
             //Console.WriteLine(Math.Truncate(y)); // afiseaza partea intreaga a numarului
             //Console.WriteLine(Math.Max(x,y)); // afiseaza cel mai mare nr dintre cele transmise ca argumente  
 
+            //Siruri de caractere
+            //char[] characters = { 'h', 'e', 'l', 'l', 'o' };
+            //string greeting = new string(characters); // se poate creea un sir de caractere dintr-un vector cu valori de tip caracter
+            //string name = "Andrei";
+            //string message = $"Hello, my name is {name} and I would like to say {greeting}!"; // interpolare de siruri
+            //Console.WriteLine(name.Length); // afiseaza lungimea sirului name
+            //Console.WriteLine(name.CompareTo("Andrei")); // compara sirul name cu sirul introdus ca argument - returneaza o valoare INTREAGA, nu BOOLEANA
+            //int position = name.IndexOf("drei"); // returneaza indicele de unde incepe subsirul argument in sirul name
+            //Console.WriteLine(name.Remove(3,2)); // elimina 2 caractere incepand cu indicele 3 -> afiseaza Andi
 
+            //Clase si obiecte
+            Person pers = new Person();
+            pers.FirstName = "Andrei";
+            pers.LastName = "Dobre";
+            //Console.WriteLine(pers.GetFullName()); //apelarea functiei GetFullName din clasa Person, specifica obiectului pers
 
+            //Structuri de decizie
+            //Console.WriteLine("Care crezi ca este numele persoanei <pers>?");
+            //string answear = Console.ReadLine();
+            //Console.WriteLine("A doua varianta?");
+            //string answear2 = Console.ReadLine();
+            //if (pers.GetFullName() == answear)
+            //{
+            //    Console.WriteLine("Raspuns corect!");
+            //}
+            //else if (pers.GetFullName() == answear2)
+            //{
+            //    Console.WriteLine("Raspuns corect, in sfarsit!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Raspuns gresit!");
+            //}
+
+            //Operatori logici si comparatii
+            //Console.WriteLine("Cati ani ai?");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Care este parola?");
+            //string password = Console.ReadLine();
+            //if(age >= 12 && password == "parola123") // varsta trebuie sa fie mai mare sau egala cu 12, iar parola trebuie sa fie "parola123"
+            //{
+            //    Console.WriteLine("Bine ai venit!"); //ambele conditii sunt adevarate
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Mai incearca!"); //cel putin o conditie este falsa
+            //}
+
+            //Switch
+            //string name = "Dobre";
+            //switch (name) // executa instructiuni in functie de valoarea argumentului
+            //{
+            //    case "Andrei":
+            //        {
+            //            Console.WriteLine("Bine ai venit!");
+            //            break; // obligatoriu pentru iesirea din switch 
+            //        }
+            //    case "Dobre":
+            //        {
+            //            Console.WriteLine("Bine ai venit, rau ai nimerit!");
+            //            return; // paraseste functia Main
+            //        }
+            //    default: // cazul de baza trateaza orice valoare neasteptata
+            //        {
+            //            Console.WriteLine("Acces restrictionat!");
+            //            break;
+            //        }
+            //}
+            //Console.WriteLine("Text dupa switch");
         }
         //void Print() // o functie trebuie declarata
         //{
