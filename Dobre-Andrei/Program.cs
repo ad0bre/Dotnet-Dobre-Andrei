69f5edefd5a46755088415ddc00a703deba4e2ce
Dobre Andrei <andrei.dobr3@gmail.com>
2021-11-06 19:49:41 +0200

    TUTORIAL: Array Sort
    Last video watched: Array Sort

--- Dobre-Andrei/Program.cs
+++ Dobre-Andrei/Program.cs
@@ -55,9 +55,9 @@ namespace Dobre_Andrei //structura de organizare
             //Console.WriteLine(name.Remove(3,2)); // elimina 2 caractere incepand cu indicele 3 -> afiseaza Andi
 
             //Clase si obiecte
-            Person pers = new Person();
-            pers.FirstName = "Andrei";
-            pers.LastName = "Dobre";
+            //Person pers = new Person();
+            //pers.FirstName = "Andrei";
+            //pers.LastName = "Dobre";
             //Console.WriteLine(pers.GetFullName()); //apelarea functiei GetFullName din clasa Person, specifica obiectului pers
 
             //Structuri de decizie
@@ -80,7 +80,7 @@ namespace Dobre_Andrei //structura de organizare
 
             //Operatori logici si comparatii
             //Console.WriteLine("Cati ani ai?");
-            //int age = Convert.ToInt32(Console.ReadLine());
+            //int age = Convert.ToInt32(Console.ReadLine()); // intputul este de tip string, asa ca trebuie convertit la tipul variabilei
             //Console.WriteLine("Care este parola?");
             //string password = Console.ReadLine();
             //if(age >= 12 && password == "parola123") // varsta trebuie sa fie mai mare sau egala cu 12, iar parola trebuie sa fie "parola123"
@@ -113,6 +113,125 @@ namespace Dobre_Andrei //structura de organizare
             //        }
             //}
             //Console.WriteLine("Text dupa switch");
+
+            //Operatorul ternar <>?<>:<>
+            // Sintaxa: <expresie>?<valoare 1>:<valoare 2>
+            //bool correct = true;
+            //int points = correct ? 10 : 0; // returneaza valoarea 1 daca expresia este adevarata sau valoarea 2 daca aceasta este falsa
+            //// poate fi echivalat cu urmatorul if:
+            //if (correct)
+            //{
+            //    points = 10;
+            //}
+            //else
+            //{
+            //    points = 0;
+            //}
+
+            //Structura if intr-o singura linie
+            //bool expresie = true;
+            //if (expresie) Console.WriteLine("True!"); // if poate contine o singura instructiune
+
+            //Structura repetitiva WHILE
+            //int i = 0; //initializare
+            //while (i < 10) // instructiunile se executa de fiecare data cand conditia este adevarata
+            //{
+            //    Console.WriteLine(i);
+            //    i++; // conditia este actualizata pentru a se putea termina dupa un numar finit de pasi, evitand un ciclu infinit
+            //}
+
+            //Structura repetitiva DO - WHILE
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i < 0); // instructiunile se vor executa cel putin o data, chiar daca conditia este falsa de la inceput
+
+            //Structura repetitiva FOR
+            //for (int i = 9; i >= 0; i--) // prima instructiune se executa o singura data inainte de intrarea in structura for, la mijloc este conditia, iar a 3-a instructiune se executa de fiecare data dupa executarea tuturor instructiunilor din blocul de cod
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Structuri imbricate
+            //bool logging = true;
+            //for (int i = 9; i >= 0; i -= 2)
+            //{
+            //    Console.WriteLine(i);
+            //    if (i == 7) // se pot utiliza structuri in interiorul altor structuri
+            //    {
+            //        if (logging == true)
+            //        {
+            //            Console.WriteLine("L-am gasit pe 7!");  
+            //        }
+            //    }
+            //}
+
+            //Structuri FOR imbricate
+            //for (int i = 9; i >= 0; i--)
+            //{
+            //    for (int k = i; k >= 0; k--) // acest for va fi parcurs de la i la 0 de 10 ori 
+            //    {
+            //        Console.Write(k + " ");
+            //    }
+            //    Console.WriteLine();
+            //} 
+
+            //Structuri WHILE imbricate
+            //int i = 9;
+            //while (i >= 0)
+            //{
+            //    int k = i;
+            //    while (k >= 0)
+            //    {
+            //        Console.Write(k + " ");
+            //        k--;
+            //    }
+            //    Console.WriteLine();
+            //    i--;
+            //}
+
+            //Tablouri
+            //int[] grades = new int[10]; //tablourile sunt obiecte ale clasei Array
+            //grades[0] = 900;
+            //int len = grades.Length; //returneaza lungimea tabloului
+            //Console.WriteLine(grades[0]);
+            //int[] grades2 = { 900, 20, 12, 41 };
+            //Console.WriteLine(grades2[4]); // va returna o eroare, deoarece s-a depasit dimensiunea
+            //int size = Convert.ToInt32(Console.In.ReadLine());
+            //int[] grades = new int[size]; 
+            //for (int i = 0; i < grades.Length; i++) // citirea unui tablou din standard input
+            //{
+            //    grades[i] = Convert.ToInt32(Console.In.ReadLine());
+            //}
+            //for (int i = 0; i < grades.Length; i++) // afisarea unui tablou in standard output
+            //{
+            //    Console.Write(grades[i] + " ");
+            //}
+            //Console.WriteLine();
+            //int[] grades = { 10, 900, 40, 230, 3 };
+            //bool found = false;
+            //for (int i = 0; i < grades.Length; i++) // structura for cauta printre elementele tabloului o anumita valoare
+            //{
+            //    if(grades[i] == 5)
+            //    {
+            //        Console.WriteLine("Gasit!");
+            //        found = true;
+            //        break; // paraseste structura for
+            //    }
+            //}
+            //if (!found) Console.WriteLine("Nu s-a gasit"); // daca elementul nu s-a gasit (found = false), se afiseaza acest mesaj
+            //Console.WriteLine(Array.IndexOf(grades, 230)); // afiseaza indicele elementului dorit din tabloul transmis ca parametru
+
+            //Sortarea tablourilor
+            //int[] grades = { 30, 430, 23, 10 };
+            //Array.Sort(grades); //sorteaza tabloul in ordine crescatoare
+            //for (int i = 0; i < grades.Length; i++)
+            //{
+            //    Console.Write(grades[i] + " ");
+            //}
+            //Console.WriteLine();
         }
         //void Print() // o functie trebuie declarata
         //{
